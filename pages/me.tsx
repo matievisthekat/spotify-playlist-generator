8dabbada491c69e1612e66dd15466de6ddac1221
MatievisTheKat <matievisthekat@gmail.com>
2021-06-29 20:39:37 +0200

    add track search feature

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import Updater from "spotify-oauth-refresher";
 import { getCreds } from "../src/util";
+import { DebounceInput } from "react-debounce-input";
+import Result from "../src/components/Result";
+import styles from "../styles/Me.module.sass";
 
 interface Props {
   id: string;
@@ -14,10 +17,13 @@ export async function getStaticProps() {
 }
 
 export default function Me({ id, secret }: Props) {
+  const updater = new Updater({ clientId: id, clientSecret: secret });
   const [error, setError] = useState("");
+  const [searchErr, setSearchErr] = useState("");
   const [username, setUsername] = useState("");
   const [url, setUrl] = useState("");
-  const updater = new Updater({ clientId: id, clientSecret: secret });
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState<SpotifyApi.TrackSearchResponse>();
 
   useEffect(() => {
     updater
@@ -32,14 +38,59 @@ export default function Me({ id, secret }: Props) {
       .catch((err) => setError(err.message));
   }, []);
 
+  useEffect(() => {
+    if (query) {
+      updater
+        .request({
+          url: "https://api.spotify.com/v1/search",
+          params: {
+            type: "track",
+            q: query,
+            limit: 30,
+            offset: 0,
+          },
+          authType: "bearer",
+        })
+        .then((res) => setResult(res.data))
+        .catch((err) => setSearchErr(err.message));
+    }
+  }, [query]);
+
   return (
     <div className="container">
       {error ? (
         <span className="error">{error}</span>
       ) : (
-        <a href={url} className="link" target="_blank">
-          <h2>{username}</h2>
-        </a>
+        <>
+          <h2>
+            Hello,&nbsp;
+            <a href={url} className="link" target="_blank">
+              {username}
+            </a>
+          </h2>
+
+          <main>
+            <span style={{ marginBottom: "1rem" }}>Search for songs and view their audio features</span>
+            <DebounceInput
+              type="text"
+              minLength={3}
+              debounceTimeout={500}
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              style={{ marginBottom: "2rem" }}
+            />
+
+            {searchErr && (
+              <span className="error" style={{ marginBottom: "1rem" }}>
+                {searchErr}
+              </span>
+            )}
+
+            <div className={styles.container}>
+              {result && result.tracks.items.map((t, i) => <Result {...t} key={i} />)}
+            </div>
+          </main>
+        </>
       )}
     </div>
   );
---
+++ src/components/Result.tsx
@@ -0,0 +1,27 @@
+import styles from "../../styles/Result.module.sass";
+
+export default function Result(props: SpotifyApi.TrackObjectFull) {
+  const onClick = () => {};
+
+  return (
+    <div className={styles.result} onClick={onClick}>
+      <img src={props.album.images[0].url} width={100} height={100} />
+      <div>
+        <span className={styles.title}>
+          <a href={props.external_urls.spotify} target="_blank">
+            {props.name}
+          </a>
+        </span>
+        <span>
+          {props.artists.map((a, i) => (
+            <small key={i} style={{ marginRight: "0.5rem" }}>
+              <a href={a.external_urls.spotify} className={styles.link} target="_blank">
+                {a.name}
+              </a>
+            </small>
+          ))}
+        </span>
+      </div>
+    </div>
+  );
+}
