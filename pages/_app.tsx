f262f40e96e60d18c285efb9528ee575607193ae
MatievisTheKat <matievisthekat@gmail.com>
2021-07-12 17:55:58 +0200

    add default seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
-import NavPath from "../src/components/NavPath";
 import type { AppProps } from "next/app";
+import NavPath from "../src/components/NavPath";
+import Seo from "../src/components/Seo";
 import "../styles/globals.sass";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Seo />
       <NavPath />
       <Component {...pageProps} />
     </>
