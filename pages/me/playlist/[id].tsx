8cfe2e38fe303b694d1808c30ce6474bcba4d9a9
MatievisTheKat <matievisthekat@gmail.com>
2021-07-15 18:05:37 +0200

    add infinite scroll for liked songs

--- pages/me/playlist/[id].tsx
+++ pages/me/playlist/[id].tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Updater from "spotify-oauth-refresher";
+import InfiniteScroll from "react-infinite-scroller";
+import { ScaleLoader } from "react-spinners";
 import ExternalLink from "../../../src/components/ExternalLink";
 import Result from "../../../src/components/Result";
 import { CredProps, getCreds, requireLogin } from "../../../src/util";
@@ -23,26 +25,66 @@ export async function getStaticProps() {
 
 export default function Playlist({ clientId, clientSecret, authUrl }: CredProps) {
   const updater = new Updater({ clientId, clientSecret });
-  const [pl, setPl] = useState<SpotifyApi.PlaylistObjectFull>();
+  const [pl, setPl] = useState<SpotifyApi.SinglePlaylistResponse>();
+  const [tracks, setTracks] = useState<SpotifyApi.PlaylistTrackObject[]>();
   const [liked, setLiked] = useState<SpotifyApi.SavedTrackObject[]>();
   const [modal, setModal] = useState(-1);
   const [loading, setLoading] = useState(true);
+  const [hasMore, setHasMore] = useState(true);
   const [error, setError] = useState("");
   const router = useRouter();
   const { id } = router.query;
   const likedSongs = id === "liked-songs";
 
+  console.log(liked?.length, tracks?.length);
+
+  const getTracks = (offset: number) => {
+    if (likedSongs) {
+      updater
+        .request<SpotifyApi.UsersSavedTracksResponse>({
+          url: "https://api.spotify.com/v1/me/tracks",
+          params: { offset, limit: 50 },
+          authType: "bearer",
+        })
+        .then(({ data }) => {
+          if (data.items.length <= 0) setHasMore(false);
+          if (offset !== 0 && liked && data.items.length > 0) setLiked([...liked, ...data.items]);
+          else setLiked(data.items);
+        })
+        .catch((err) => setError(err.message));
+    } else {
+      updater
+        .request<SpotifyApi.PlaylistTrackResponse>({
+          url: `https://api.spotify.com/v1/playlists/${id}/tracks`,
+          params: { offset, limit: 50 },
+          authType: "bearer",
+        })
+        .then(({ data }) => {
+          if (data.items.length <= 0) setHasMore(false);
+          if (offset !== 0 && tracks && data.items.length > 0) setTracks([...tracks, ...data.items]);
+          else setTracks(data.items);
+        })
+        .catch((err) => setError(err.message));
+    }
+  };
+
+  const loadMore = (page: number) => getTracks(50 * page);
+
   useEffect(() => {
     requireLogin(updater, authUrl);
 
-    updater
-      .request({
-        url: likedSongs ? "https://api.spotify.com/v1/me/tracks" : `https://api.spotify.com/v1/playlists/${id}`,
-        authType: "bearer",
-      })
-      .then(({ data }) => (likedSongs ? setLiked(data.items) : setPl(data)))
-      .catch((err) => setError(err.message))
-      .finally(() => setLoading(false));
+    if (!likedSongs) {
+      updater
+        .request<SpotifyApi.SinglePlaylistResponse>({
+          url: `https://api.spotify.com/v1/playlists/${id}`,
+          authType: "bearer",
+        })
+        .then(({ data }) => setPl(data))
+        .catch((err) => setError(err.message))
+        .finally(() => setLoading(false));
+    }
+
+    getTracks(0);
   }, []);
 
   return (
@@ -72,32 +114,41 @@ export default function Playlist({ clientId, clientSecret, authUrl }: CredProps)
         </span>
       )}
       {error && <span className="error">{error}</span>}
-      {pl && (
+      {(tracks || liked) && (
         <div className={styles.tracks}>
-          {pl.tracks.items.map((t, i) => (
-            <Result
-              {...t}
-              setShowModal={(v: boolean) => setModal(v ? i : -1)}
-              showModal={modal === i}
-              updater={updater}
-              key={i}
-              compact
-            />
-          ))}
-        </div>
-      )}
-      {liked && (
-        <div className={styles.tracks}>
-          {liked.map((t, i) => (
-            <Result
-              {...t}
-              setShowModal={(v: boolean) => setModal(v ? i : -1)}
-              showModal={modal === i}
-              updater={updater}
-              key={i}
-              compact
-            />
-          ))}
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={loadMore}
+            hasMore={hasMore}
+            loader={
+              <div style={{ textAlign: "center" }}>
+                <ScaleLoader loading={true} color="#1DB954" />
+              </div>
+            }
+          >
+            {tracks &&
+              tracks.map((t, i) => (
+                <Result
+                  {...t}
+                  setShowModal={(v: boolean) => setModal(v ? i : -1)}
+                  showModal={modal === i}
+                  updater={updater}
+                  key={i}
+                  compact
+                />
+              ))}
+            {liked &&
+              liked.map((t, i) => (
+                <Result
+                  {...t}
+                  setShowModal={(v: boolean) => setModal(v ? i : -1)}
+                  showModal={modal === i}
+                  updater={updater}
+                  key={i}
+                  compact
+                />
+              ))}
+          </InfiniteScroll>
         </div>
       )}
     </div>
