bc8eb2de1595a6e442cb2ffc27135386866947cf
MatievisTheKat <matievisthekat@gmail.com>
2021-07-19 13:57:56 +0200

    add progress bar for route changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 import type { AppProps } from "next/app";
+import Router from "next/router";
+import progress from "nprogress";
 import NavPath from "../src/components/NavPath";
 import Seo from "../src/components/Seo";
 import "../styles/globals.sass";
+
+progress.configure({
+  minimum: 0.3,
+  easing: "ease",
+  speed: 800,
+  showSpinner: false,
+});
+
+Router.events.on("routeChangeStart", () => progress.start());
+Router.events.on("routeChangeComplete", () => progress.done());
+Router.events.on("routeChangeError", () => progress.done());
 
 function App({ Component, pageProps }: AppProps) {
   return (
