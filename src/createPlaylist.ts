028988b73b0b35da2f3a7fbad90b0198e2b644e9
matievisthekat <matievisthekat@gmail.com>
2021-12-30 13:22:41 +0200

    add createPlaylist function

---
+++ src/createPlaylist.ts
@@ -0,0 +1,57 @@
+import Updater from "spotify-oauth-refresher";
+
+interface Args {
+  tracks: string[];
+  desc?: string;
+  pub?: boolean;
+  onProgress: (percent: number) => void;
+  onDone: () => void;
+}
+
+export const createPlaylist = (
+  name: string,
+  userId: string,
+  { tracks, desc, pub, onProgress, onDone }: Args,
+  updater: Updater
+) => {
+  return new Promise<string>((resolve, reject) => {
+    updater
+      .request<SpotifyApi.CreatePlaylistResponse>({
+        method: "POST",
+        url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+        data: {
+          name,
+          public: pub,
+          collaborative: false,
+          description: desc,
+        },
+        authType: "bearer",
+      })
+      .then(({ data }) => {
+        resolve(data.id);
+
+        const totalChunks = Math.ceil(tracks.length / 100);
+        let currentChunk = 0;
+
+        const interval: NodeJS.Timer = setInterval(() => {
+          if (currentChunk > totalChunks) {
+            clearInterval(interval);
+            onDone();
+            return;
+          }
+
+          const chunk = tracks.slice(currentChunk * 100, (currentChunk + 1) * 100);
+          updater
+            .request<SpotifyApi.AddTracksToPlaylistResponse>({
+              method: "POST",
+              url: `https://api.spotify.com/v1/playlists/${data.id}/tracks`,
+              data: { uris: chunk },
+              authType: "bearer",
+            })
+            .then(() => onProgress((currentChunk / totalChunks) * 100))
+            .catch((err) => reject(err.response.data.error.message || err));
+        }, 1000);
+      })
+      .catch((err) => reject(err.response.data.error.message || err));
+  });
+};
