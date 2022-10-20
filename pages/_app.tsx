701593c9126abea35164bd01a02977e08eacc6eb
matievisthekat <matievisthekat@gmail.com>
2022-10-20 09:27:39 +0200

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import Router from "next/router";
+import Script from "next/script";
 import progress from "nprogress";
 import NavPath from "../src/components/NavPath";
 import Seo from "../src/components/Seo";
@@ -17,11 +18,25 @@ Router.events.on("routeChangeComplete", () => progress.done());
 Router.events.on("routeChangeError", () => progress.done());
 
 function App({ Component, pageProps }: AppProps) {
+
+
   // TODO: clear this bug up
   const Comp = Component as any;
   return (
     <>
       <Seo />
+      
+      <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-7DMRTCSBEV"></Script>
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-7DMRTCSBEV');
+        `}
+      </Script>
+      
       <NavPath />
       <Comp {...pageProps} />
     </>
