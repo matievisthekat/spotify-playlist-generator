2885848eb01456ece3bb47f7264e826b9b2b9abb
matievisthekat <matievisthekat@gmail.com>
2022-11-01 18:03:12 +0200

    convert to Script comp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,13 +26,22 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Seo />
-
-      <Head>
-        <script id="google-ad-sense" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4374857081025109" crossOrigin="anonymous"></script>
-      </Head>
       
-      <script id="google-tag-manager" defer src="https://www.googletagmanager.com/gtag/js?id=G-7DMRTCSBEV"></script>
-      <Script id="google-analytics" defer>
+      <Script
+        id="google-adsense"
+        data-ad-client="ca-pub-4374857081025109"
+        async={true}
+        strategy="beforeInteractive"
+        crossOrigin="anonymous"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4374857081025109"
+        onError={(e) => console.error('AdSense script failed to load: ', e)}
+      ></Script>
+      <Script
+        id="google-tag-manager"
+        strategy="beforeInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-7DMRTCSBEV"
+      ></Script>
+      <Script id="google-analytics" strategy="beforeInteractive">
         {`window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
