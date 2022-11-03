f1326ac76fb1c5bd9fd8ebb9fd4ebf34127e35fc
matievisthekat <matievisthekat@gmail.com>
2022-11-03 13:11:17 +0200

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,17 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          id="google-tag-manager"
+          strategy="beforeInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-7DMRTCSBEV"
+        ></Script>
+        <Script id="google-analytics" strategy="beforeInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-7DMRTCSBEV');`}
+        </Script>
       </body>
     </Html>
   );
